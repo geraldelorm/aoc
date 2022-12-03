d244dcadf0ab4747668c60c5644f0e02270281be
Gerald Elorm Gbagbe <gerald.elorm.gbagbe@gmail.com>
2022-12-03 00:25:19 +0000

    aoc-02

--- day02/day2.py
+++ day02/day2.py
@@ -32,3 +32,75 @@ def p2(file):
 
 print("P2 Test: ", p2("day02/test.txt"))
 print("P2 Actual: ", p2("day02/day2.txt"))
+
+
+
+
+
+# def p1(file):
+#     with open(file) as f:
+#         score = 0
+#         for line in f:
+#             opponent_move, my_move = line.strip().split(" ")
+#             if opponent_move == "A":
+#                 if my_move == "X":
+#                     score += 1 + 3
+#                 elif my_move == "Y":
+#                     score += 2 + 6
+#                 elif my_move == "Z":
+#                     score += 3 + 0
+
+#             elif opponent_move == "B":
+#                 if my_move == "X":
+#                     score += 1 + 0
+#                 elif my_move == "Y":
+#                     score += 2 + 3
+#                 elif my_move == "Z":
+#                     score += 3 + 6
+
+#             elif opponent_move == "C":
+#                 if my_move == "X":
+#                     score += 1 + 6
+#                 elif my_move == "Y":
+#                     score += 2 + 0
+#                 elif my_move == "Z":
+#                     score += 3 + 3
+#         return score
+
+# print("P1 Test: ", p1("day02/test.txt"))
+# print("P1 Actual: ", p1("day02/day2.txt"))
+
+
+
+# def p2(file):
+#     with open(file) as f:
+#         score = 0
+#         for line in f:
+#             opponent_move, my_move = line.strip().split(" ")
+#             if opponent_move == "A":
+#                 if my_move == "X":
+#                     score += 3 + 0
+#                 elif my_move == "Y":
+#                     score += 1 + 3
+#                 elif my_move == "Z":
+#                     score += 2 + 6
+
+#             elif opponent_move == "B":
+#                 if my_move == "X":
+#                     score += 1 + 0
+#                 elif my_move == "Y":
+#                     score += 2 + 3
+#                 elif my_move == "Z":
+#                     score += 3 + 6
+
+#             elif opponent_move == "C":
+#                 if my_move == "X":
+#                     score += 2 + 0
+#                 elif my_move == "Y":
+#                     score += 3 + 3
+#                 elif my_move == "Z":
+#                     score += 1 + 6
+#         return score
+
+# print("P2 Test: ", p2("day02/test.txt"))
+# print("P2 Actual: ", p2("day02/day2.txt"))
